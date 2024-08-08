680dbc6b0e12bc5d48f776a761678382da1389ed
tangzl <1240837715@qq.com>
2024-08-08 17:45:05 +0800

    文案修改

--- src/components/ApiSearch.vue
+++ src/components/ApiSearch.vue
@@ -6,7 +6,7 @@ const handleReleaseAll = inject(RELEASE_ALL_API)
 <template>
   <div flex items-center>
     <el-input v-model="value" :prefix-icon="Search" w-full style="--el-color-primary: var(--primary);" clearable />
-    <el-tooltip effect="light" content="列表的接口将全部放行" placement="bottom-end">
+    <el-tooltip effect="light" content="将放行未拦截的请求" placement="bottom-end">
       <el-icon ml-1.25 size="20" color="#9373EE" @click="handleReleaseAll?.()" cursor-pointer>
         <Brush />
       </el-icon>
